91260802c9aa08d19e3e336af2bd7a58f8935352
DouglasH9 <douglash9882@gmail.com>
2022-06-06 14:34:49 -0500

    added db context

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_dev_lessons.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
